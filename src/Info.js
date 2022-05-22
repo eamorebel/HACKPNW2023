01d745a6fc7c88a2d488be13c89c3516af676005
Daniel Xiao <danielxiao@Daniels-MacBook-Air.local>
2022-05-21 23:09:04 -0500

    Finished code

--- src/Info.js
+++ src/Info.js
@@ -1,55 +1,38 @@
 import React from 'react'
-import CountUp from 'react-countup';
 const Info = () => {
     
   return (
       <div class="">
         <div class="mb-10">
-            <CountUp
-                start={0}
-                end={81}
-                duration={4}
-                >
-                {({ countUpRef}) => (
+            
                     <div>
-                    <span class="text-red-500" ref={countUpRef} />
+                    <span class="text-red-500">
+                        81
+                    </span>
                     % of US Adults would be interested in technology that can sense elderly falls
                     </div>
-                )}
-            </CountUp>
         </div>
         <div class="mb-10">
-        <CountUp
-            start={0}
-            end={13}
-            duration={4}
-            prefix=" "
-            suffix=" "
-            >
-            {({ countUpRef}) => (
+        
                 <div>
-                    Every
-                <span class="text-red-500" ref={countUpRef} />
+                    Every&nbsp;
+                <span class="text-red-500">
+                    13&nbsp;
+                </span>
                 seconds an older adult is sent to the ER after a fall
                 </div>
-            )}
-        </CountUp>
+            
         </div>
         <div class="mb-10">
-        <CountUp
-            start={0}
-            end={50}
-            duration={4}
-            suffix=" "
-            >
-            {({ countUpRef}) => (
+        
                 <div>
                     In 2015, the total medical costs for falls totaled more than $
-                <span class="text-red-500" ref={countUpRef} />
-                billion
+                <span class="text-red-500">
+                    50&nbsp;
+                </span>
+                 billion
                 </div>
-            )}
-        </CountUp>
+            
         </div>
       
       </div>
